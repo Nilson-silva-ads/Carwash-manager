17cbe005511231a92c50bb873926f626366745f6
Nilson Silva <nilsonsilva.ads00@gmail.com>
2026-07-15 00:04:38 -0300

    feat:(models): add base project models

---
+++ app/models/base.model.py
@@ -0,0 +1,24 @@
+from datetime import datetime, timezone
+
+from sqlachemy import Datetime
+from sqlalchemy.orm import Mapped, mapped_column
+
+from app.database.base import Base
+
+
+class BaseModel(Base):
+    """ Classe base para todos os modelos de aplicação. """
+    __abstract__=True #Diz ao sqlalchemy para não criar uma tabela chamada base_model.
+
+    id: Mapped[int] = mapped_column(primary_key=True) #noda modelagem para melhorar a tipagem, criando o id.
+
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True),
+        default=lambda: datetime.now(timezone.utc), #defalut: sempre que um novo registro for criado o SQLAlchemy, preenceherar automaticamente a data e hora atuais.
+    )
+
+    updated_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True),
+        default=lambda: datetime.now(timezone.utc),
+        onupdate=lambda: datetime.now(timezone.utc), #onupdate: sempre que um registro for alterado, o campo update_at sera atualizado automaticamente.
+    )
