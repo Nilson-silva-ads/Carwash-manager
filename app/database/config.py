6d8748c8dd50e35d871ab4d7964991ddacc7e445
Nilson Silva <nilsonsilva.ads00@gmail.com>
2026-09-02 23:23:31 -0300

    prepara arquivos de configuração para deploy

--- app/database/config.py
+++ app/database/config.py
@@ -1,1 +1,13 @@
-DATABASE_URL = "sqlite:///data/carwash.db"
+import os
+
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+
+if not DATABASE_URL:
+    raise ValueError("DATABASE_URL não definida.")
